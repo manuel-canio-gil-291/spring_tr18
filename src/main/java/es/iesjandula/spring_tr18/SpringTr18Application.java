79c14711ee15b78486bd4665b1b4187623041b67
Manuel Cano Gil <mcangil786@g.educaand.es>
2023-01-11 12:52:10 +0100

    First implementation of the project

---
+++ src/main/java/es/iesjandula/spring_tr18/SpringTr18Application.java
@@ -0,0 +1,13 @@
+package es.iesjandula.spring_tr18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTr18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTr18Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/iesjandula/spring_tr18/SpringTr18ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iesjandula.spring_tr18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTr18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
