66bb009ae211cc34d7e67e071224566e7d7595c6
Manuel Cano Gil <mcangil786@g.educaand.es>
2023-01-11 20:52:51 +0100

    Creating the design of the web page

--- src/main/java/es/iesjandula/spring_tr18/controller/WebController.java
+++ src/main/java/es/iesjandula/spring_tr18/controller/WebController.java
@@ -1,9 +1,19 @@
 package es.iesjandula.spring_tr18.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import es.iesjandula.spring_tr18.models.Profesor;
 
 @Controller
 public class WebController 
 {
-        
+    @RequestMapping(method = RequestMethod.GET, value = "/login")
+    public String login(Model model)
+    {
+        model.addAttribute("teacher", new Profesor());
+        return "identificacion.html";
+    }  
 }
